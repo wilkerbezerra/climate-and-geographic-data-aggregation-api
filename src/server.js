352a701757dc788e86dbb9279bc111807cde25e4
Wilker Silva <wilker.bezerra1000@gmail.com>
2026-05-05 11:04:47 -0300

    feat: atualização do arquivo server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const cors = require("cors");
+const routes = require("./routes.js");
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+// app.use("/api/v1", routes);
 
 app.get("/", (req, res) => {
   res.send("Servidor rodando!!!!");
